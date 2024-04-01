2b4ee862f90088aea0a946e0e40a60958daea6aa
Dartagnan Valarini <dartfreire@gmail.com>
2024-04-01 14:53:02 -0300

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -1,13 +1,26 @@
 import pandas as pd
 import streamlit as st
+import requests
 
 st.set_page_config(
     page_title='Home',
     layout='wide'
 )
 
+# URL do arquivo CSV no GitHub
+url_csv = "https://raw.githubusercontent.com/seu_usuario/seu_repositorio/master/teste/teste.csv"
+
+# Fazer a requisição HTTP para obter o conteúdo do arquivo CSV
+resposta = requests.get(url_csv)
+
+# Verificar se a requisição foi bem sucedida
+if resposta.status_code == 200:
+    # Ler o conteúdo do arquivo CSV usando o pandas
+    df = pd.read_csv(resposta.content)
+
+
 if "data" not in st.session_state:
-    df = pd.read_csv('https://github.com/DartagnanFreire/UT_supertrunfo/blob/main/arquivos%20csv/ultimateteam.csv', sep='\t', encoding='utf-8')
+    df = df
     st.session_state["data"] = df
     
 
