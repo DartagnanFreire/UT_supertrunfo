743215a144143ee2c792124b30104e2465c43ba3
Dartagnan Valarini <dartfreire@gmail.com>
2024-04-01 14:54:03 -0300

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -7,20 +7,16 @@ st.set_page_config(
     layout='wide'
 )
 
-# URL do arquivo CSV no GitHub
-url_csv = "https://raw.githubusercontent.com/seu_usuario/seu_repositorio/master/teste/teste.csv"
-
-# Fazer a requisição HTTP para obter o conteúdo do arquivo CSV
-resposta = requests.get(url_csv)
-
-# Verificar se a requisição foi bem sucedida
-if resposta.status_code == 200:
-    # Ler o conteúdo do arquivo CSV usando o pandas
-    df = pd.read_csv(resposta.content)
 
 
 if "data" not in st.session_state:
-    df = df
+    # URL do arquivo CSV no GitHub
+    url_csv = "https://raw.githubusercontent.com/seu_usuario/seu_repositorio/master/teste/teste.csv"
+
+    # Fazer a requisição HTTP para obter o conteúdo do arquivo CSV
+    resposta = requests.get(url_csv)
+
+    df = pd.read_csv(resposta.content)
     st.session_state["data"] = df
     
 
